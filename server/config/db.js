fcf4dfe7b5bc0df767ecf1b793a7d5dd15c8668f
cathyai0320 <cathystudyingai@gmail.com>
2023-03-09 23:13:34 -0500

    initial commit

---
+++ server/config/db.js
@@ -0,0 +1,13 @@
+/******************************
+ * File: db.js
+ * Author: 
+ * Student ID: 301284811
+ * Web app: Favourite Book List
+ ******************************/
+module.exports =
+ {
+
+    "URI":"mongodb+srv://bernice:LHkZmAq6HD06ptqE@cluster0.sgfemky.mongodb.net/test"
+}
+
+//mongodb+srv://bernice:<password>@cluster0.sgfemky.mongodb.net/test
